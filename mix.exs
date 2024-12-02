1c48fd0884505ae5603ac4f472c556180d89d043
Justin Johnson <justin@justin.pub>
2024-12-02 15:51:26 -0600

    Version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Decent.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :decent,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
